70af023f63bb8b54d04f135b1f293a61f422693a
UNILORN <b5159@oic.jp>
2016-11-20 20:24:45 +0900

    blade更新

---
+++ resources/views/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
+    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
+    <link rel="stylesheet" href="/css/@yield('link')" media="screen" title="no title">
+    <script src="/js/@yield('script')"></script>
+  </head>
+  <body>
+    <header>
+
+
+    </header>
+
+    @yield('content')
+
+    <footer>
+
+
+    </footer>
+
+    
+  </body>
+</html>
---
+++ resources/views/cart.blade.php
@@ -0,0 +1,11 @@
+@extends('app')
+
+@section('title','MyCart')
+@section('link','')
+@section('script','')
+
+@section('content')
+  <main>
+    
+  </main>
+@endsection
