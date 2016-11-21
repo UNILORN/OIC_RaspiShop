73eeb8858dd4aad389052cd4779fda3e65f0114a
UNILORN <b5159@oic.jp>
2016-11-21 12:11:35 +0900

    view

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -2,10 +2,19 @@
 
 @section('title','Costmize')
 @section('link','product.css')
-@section('script','')
+@section('script','product.js')
 
 @section('content')
+  <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
   <main class="container">
+
+    <ul id="slide-out" class="side-nav">
+      <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+      <li><a href="#!">Second Link</a></li>
+      <li><div class="divider"></div></li>
+      <li><a class="subheader">Subheader</a></li>
+      <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    </ul>
 
     <form action="/product" method="post">
 
@@ -77,6 +86,13 @@
         @endforeach
       </div>
 
+      <div class="row formfooter z-depth-5 valign-wrapper">
+        <div class="container">
+          <div class="col s6 valign"></div>
+          <div class="col s3 valign"><span class="flow-text right">¥20000</span></div>
+          <div class="col s3 valign"><input type="submit" class="waves-effect waves-light btn" value="購入する"></div>
+        </div>
+      </div>
     </form>
 
   </main>
