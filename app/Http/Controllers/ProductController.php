1a6ae4e3e8dea471c800e62084ce515d887644a7
Unilorn <b5159@oic.jp>
2016-12-15 13:24:05 +0900

    最新版

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\MST_PRODUCT;
 use App\MST_COMPANY;
+use App\Service\CartService;
 
 class ProductController extends Controller
 {
@@ -14,22 +15,10 @@ class ProductController extends Controller
         $sdcard = MST_PRODUCT::where('type_id', 2)->get();
         $os = MST_PRODUCT::where('type_id', 3)->get();
 
-        $cart = session()->get('cart',[]);
-        $allsumprice = 0;
-        $sumprice = [];
-        if (!empty($cart)) {
-            foreach ($cart as $key => $value) {
-                $sumprice[$key] = 0;
-                $cart[$key]['raspi'] = MST_PRODUCT::find($cart[$key]['raspi']);
-                $cart[$key]['os'] = MST_PRODUCT::find($cart[$key]['os']);
-                $cart[$key]['sdcard'] = MST_PRODUCT::find($cart[$key]['sdcard']);
-                foreach ($cart[$key] as $innerkey => $innervalue){
-                    $allsumprice += $innervalue->price;
-                    $sumprice[$key]    += $innervalue->price;
-                }
-
-            }
-        }
+        $c_cart = new CartService();
+        $cart        = $c_cart->getItem();
+        $allsumprice = $c_cart->getAllPrice();
+        $sumprice    = $c_cart->getPrice();
 
         return view('product', compact('raspi', 'sdcard', 'os', 'request', 'cart' , 'allsumprice', 'sumprice'));
     }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -60,6 +60,41 @@
                             </div>
                         </div>
 
+                        {{--<div class="form-group">--}}
+                            {{--<label for="sex" class="col-md-4 control-label">性別</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<select id="sex" name="sex" class="form-control" required>--}}
+                                    {{--<option value="1">男性</option>--}}
+                                    {{--<option value="2">女性</option>--}}
+                                {{--</select>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<label for="phone" class="col-md-4 control-label">電話番号</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="phone" type="text" class="form-control" name="phone" required>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<label for="post-num" class="col-md-4 control-label">郵便番号</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="post-num" type="text" class="form-control" name="post_num" required>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
+                        {{--<div class="form-group">--}}
+                            {{--<label for="address" class="col-md-4 control-label">住所</label>--}}
+
+                            {{--<div class="col-md-6">--}}
+                                {{--<input id="address" type="text" class="form-control" name="address" required>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
